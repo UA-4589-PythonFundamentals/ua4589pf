f9b61ceb70860f5ff8e09100030b384ac7c90bab
Mark Kostyria <mark12062004@gmail.com>
2025-09-03 17:13:54 +0300

    hw8-Kostyria Mark

---
+++ hw/hw08/Task3/geometry.py
@@ -0,0 +1,10 @@
+import math
+
+def area_rectangle(a, b):
+    return a * b
+
+def area_triangle(base, height):
+    return 0.5 * base * height
+
+def area_circle(radius):
+    return math.pi * math.pow(radius, 2)
