1e9d17260da3c4ff09c47b63bb6e01b8ee0212f1
Max Kudyk <138608776+Neplyx@users.noreply.github.com>
2025-09-04 22:12:06 +0300

    Update main.py

--- hw/hw08/Max Kudyk/task3/main.py
+++ hw/hw08/Max Kudyk/task3/main.py
@@ -1,15 +1,17 @@
 import area
-print(dir(area))
-print(area.__file__)
 
 def main():
     choose = int(input("Rectangle - 1\nTriangle - 2\nCircle - 3\nYour choice: "))
+    
     if choose == 1:
         area.area_rectangle()
+        
     elif choose == 2:
         area.area_triangle()
+        
     elif choose == 3:
         area.area_circle()
+        
     else:
         print("Wrong input")
 
