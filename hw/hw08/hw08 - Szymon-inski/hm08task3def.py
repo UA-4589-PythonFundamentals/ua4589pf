3fde4c905b58707bb6cf497b1febf8e5a94a6124
Szymon Muchiński <szymon.muchin@gmail.com>
2025-09-06 20:42:44 +0200

    add task 3

--- hw/hw08/hw08 - Szymon-inski/hm08task3def.py
+++ hw/hw08/hw08 - Szymon-inski/hm08task3def.py
@@ -4,17 +4,21 @@ def s_triangle():
     x = int(input('Write the length: '))
     y = int(input('Write the height: '))
     result = (x * y) / 2
+    print(f'Area of triangle is {result}')
     return result
+
 
 def s_circle():
         x = int(input('Write the radius:'))
-        result = 
+        result = pi * pow(x, 2)
+        print(f'Area of circle is {result}')
         return result
 
 def s_rectangle():
     x = int(input('Write the length:'))
     y = int(input('Write the width:'))
     result = x * y
+    print(f'Area of rectangle is {result}')
     return result
 
 
