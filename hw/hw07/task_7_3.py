2bf2c9f647141ce04d6a41d2b11522a4baa87bd4
inFERRNOO <weblab24x7@gmail.com>
2025-09-11 18:51:04 +0000

    completed tsks 7_1-7_3

--- hw/hw07/task_7_3.py
+++
@@ -1,8 +0,0 @@
-def number_of_characters(word):
-    num_of_char = dict()
-    for i in word:
-        num_of_char[i] = num_of_char.get(i, 0) + 1
-    return num_of_char
-
-word = input("Enter a word: ")
-print(number_of_characters(word))
