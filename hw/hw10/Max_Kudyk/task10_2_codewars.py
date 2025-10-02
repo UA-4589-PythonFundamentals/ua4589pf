25296d68bcd2630c6b9cff09c6158eb71ea182e5
Max Kudyk <kydukm1@gmail.com>
2025-10-02 14:19:17 +0300

    update codewars task

--- hw/hw10/Max_Kudyk/task10_2_codewars.py
+++ hw/hw10/Max_Kudyk/task10_2_codewars.py
@@ -6,66 +6,77 @@
 #--------------------------------------------
 
 #TASK 2()
-class Ghost(object):
-    counter = 0
-    def __init__(self):
-        colors=["white","yellow","purple","red"]
-        self.color = colors[Ghost.counter % len(colors)]
-        Ghost.counter += 1
-#--------------------------------------------
+# class Ghost(object):
+#     counter = 0
+#     def __init__(self):
+#         colors=["white","yellow","purple","red"]
+#         self.color = colors[Ghost.counter % len(colors)]
+#         Ghost.counter += 1
+# #--------------------------------------------
 
-#TASK 3()
-class Human:
-    pass
 
-class Man(Human):
-    pass
+# #TASK 3()
+# class Human:
+#     pass
 
-class Woman(Human):
-    pass
+# class Man(Human):
+#     pass
 
-def God():
-    return [Man(), Woman()]
-#--------------------------------------------
+# class Woman(Human):
+#     pass
 
-#TASK 4()
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.info = f"{name}s age is {age}"
+# def God():
+#     return [Man(), Woman()]
+# #--------------------------------------------
 
-#--------------------------------------------
+# #TASK 4()
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         self.info = f"{name}s age is {age}"
 
-#TASK 5()
-import math
+# #--------------------------------------------
 
-class Sphere:
-    def __init__(self, radius, mass):
-        self.radius = radius
-        self.mass = mass
+# #TASK 5()
+# import math
 
-    def get_radius(self):
-        return self.radius
+# class Sphere:
+#     def __init__(self, radius, mass):
+#         self.radius = radius
+#         self.mass = mass
 
-    def get_mass(self):
-        return self.mass
+#     def get_radius(self):
+#         return self.radius
 
-    def get_volume(self):
-        volume = (4 / 3) * math.pi * (self.radius ** 3)
-        return round(volume, 5)
+#     def get_mass(self):
+#         return self.mass
 
-    def get_surface_area(self):
-        surface_area = 4 * math.pi * (self.radius ** 2)
-        return round(surface_area, 5)
+#     def get_volume(self):
+#         volume = (4 / 3) * math.pi * (self.radius ** 3)
+#         return round(volume, 5)
 
-    def get_density(self):
-        density = self.mass / ((4 / 3) * math.pi * (self.radius ** 3))
-        return round(density, 5)
+#     def get_surface_area(self):
+#         surface_area = 4 * math.pi * (self.radius ** 2)
+#         return round(surface_area, 5)
+
+#     def get_density(self):
+#         density = self.mass / ((4 / 3) * math.pi * (self.radius ** 3))
+#         return round(density, 5)
 
 #--------------------------------------------
 
 #TASK 6()
+#def rename_class(cls, new_name):
+#
+#    if not new_name:
+#        raise ValueError("Class name cannot be empty")
+#    if not new_name[0].isupper():
+#        raise ValueError("Class name must start with an uppercase letter")
+#    if not new_name.isalnum():
+#        raise ValueError("Class name must contain only alphanumeric characters")
 
+#    cls.__name__ = new_name
+#    return cls
 #--------------------------------------------
 
