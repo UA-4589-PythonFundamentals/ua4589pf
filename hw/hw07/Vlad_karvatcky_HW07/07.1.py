beb0a9df31b1b8f97bd446b3ecc217378e45355e
wertt599 <vlad.karvatckyy@gmail.com>
2025-10-11 21:30:43 +0200

    Add files via upload

---
+++ hw/hw07/Vlad_karvatcky_HW07/07.1.py
@@ -0,0 +1,7 @@
+def largest_of_two_nums(num1, num2):    
+    """this function returns largest of two numbers"""
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+print(largest_of_two_nums(12, 24))
